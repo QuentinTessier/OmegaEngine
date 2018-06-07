ed57dd683a0bd2438cee612922df2e825de82a98
Tessier Quentin <tquentin@localhost.localdomain>
2018-06-07 03:45:29 +0200

    Packing the initialisation of the map struct in a function

---
+++ src/map.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2018
+** CSFML_2D_ENGINE
+** File description:
+** map
+*/
+
+#include <stdlib.h>
+#include "engine.h"
+
+map_t init_map(win_t *win_info)
+{
+	map_t map;
+	
+	map.v_map = generate_vector(win_info);
+	map.w_x = 0;
+	map.w_y = 0;
+	map.d_map = generator(win_info->sq_x, win_info->sq_y, 0, 0);
+	map.c_map = generate_convex(win_info, map.v_map);
+	map.tex = create_texture();
+	return (map);
+}
