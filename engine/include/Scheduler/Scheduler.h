8e58c021763c286bc7c6a58dfd061d3e88050c75
quentin.tessier@epitech.eu <quentin.tessier@epitech.eu>
2019-03-25 16:44:20 +0100

    Removing the current Scheduler struct rethinking it's purpose and the code

--- engine/include/Scheduler/Scheduler.h
+++ engine/include/Scheduler/Scheduler.h
@@ -6,30 +6,3 @@
 */
 
 #pragma once
-
-#include "Component.h"
-
-enum OmSchedulerReadManner {
-    Read_First,
-    Read_Last
-};
-
-typedef struct OmScheduler {
-    OmComponent Comp[1];
-
-    OmVector *stack;
-    int ReadManner;
-} OmScheduler;
-
-struct _OmScheduler {
-    OmScheduler *(* const Create)(void);
-    void (* const Destroy)(OmScheduler *item);
-    OmScheduler *(* const Init)(OmScheduler *item, int ReadManner);
-    bool (* const Add)(OmScheduler *item, void *data);
-    void *(* const Pop)(OmScheduler *item);
-    void *(* const Read)(OmScheduler *item,
-            void (* func)(void *context, void *elem, size_t idx),
-            void *context);
-};
-
-extern _OmScheduler OmShedulerS[1];
